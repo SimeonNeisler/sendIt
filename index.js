87f3d9d7dc45fd71149463c48a19b027185a05b4
Simeon Neisler <simeon.neisler@gmail.com>
2017-10-10 16:55:45 -0400

    Stood up first part of authentication

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
+//npm packages
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+//private config settings
+const keys = require('./config/keys');
+
+require('./models/user');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'There'});
-});
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 3000;
 
